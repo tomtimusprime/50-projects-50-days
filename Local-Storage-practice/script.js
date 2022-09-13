2b1803b0997160aa436594727ffdf460b8fd62f0
Tom Black <tom.black06007@gmail.com>
2022-09-12 22:08:25 -0600

    Got the radio button values saving correctly.

--- Local-Storage-practice/script.js
+++ Local-Storage-practice/script.js
@@ -4,14 +4,19 @@ const submitFunction = (event) => {
     let firstName = document.getElementById('first-name').value;
     let lastName = document.getElementById('last-name').value;
     let email = document.getElementById('email').value;
-    let gender = document.getElementById('gender').value;
+    let radios = document.getElementsByName('gender');
     let birthdate = document.getElementById('birthdate').value;
     let additionalNotes = document.getElementById('additionalNotes').value;
+
+    for(let radio of radios){
+        if(radio.checked){
+            localStorage.setItem('ls_gender', radio.value);
+        }
+    }
 
     localStorage.setItem('ls_first_name', firstName);
     localStorage.setItem('ls_last_name', lastName);
     localStorage.setItem('ls_email', email);
-    localStorage.setItem('ls_gender', gender);
     localStorage.setItem('ls_birthdate', birthdate);
     localStorage.setItem('ls_additionalNotes', additionalNotes);
 }
