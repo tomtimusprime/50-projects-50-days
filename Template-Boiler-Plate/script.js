4f4fa08939b331f39fd5e337fed305fc4fe82111
Tom Black <tom.black06007@gmail.com>
2022-09-13 14:58:47 -0600

    Added some dom manipulation notes to the boiler plate folder.

---
+++ Template-Boiler-Plate/script.js
@@ -0,0 +1,8 @@
+//Dom Manipulation Notes
+//There are 5 ways to select items in the DOM
+
+//1. getElementById();
+//2. getElementByClassName();
+//3. getElementsByTagName(); Returns an HTMLElement List
+//4. querySelector();
+//5. querySelectorAll(); Returns a nodelist
