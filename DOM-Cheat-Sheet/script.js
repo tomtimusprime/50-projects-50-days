8ad28d0505c45b55e2560c6673b8bc6722df4f99
Tom Black <tom.black06007@gmail.com>
2022-09-14 17:01:39 -0600

    Adding a DOM manipulation Cheat Sheet for future reference.

---
+++ DOM-Cheat-Sheet/script.js
@@ -0,0 +1,12 @@
+//Dom Manipulation Notes
+//There are 5 ways to select items in the DOM
+
+//1. getElementById();
+//2. getElementByClassName();
+//3. getElementsByTagName(); Returns an HTMLElement List
+//4. querySelector();
+//5. querySelectorAll(); Returns a nodelist
+
+//First DOM technique to master DOM manipulation - web dev simplified https://www.youtube.com/watch?v=y17RuWkWdn8 (Learn DOM Manipulation in 18 minutes)
+
+//1. Adding elements to a page:
